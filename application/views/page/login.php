c7b9e130c28d03d2389f8b31537d529dd85a1a1a
Shoichiinaba <shoichiinaba01@gmail.com>
2023-06-05 09:47:31 +0700

    update navbar sitmep client & update tampilan halaman login

--- application/views/page/login.php
+++ application/views/page/login.php
@@ -39,6 +39,7 @@
                     <div class="col-xl-4 col-lg-5 col-md-7 mx-auto">
                         <div class="card z-index-0">
                             <div class="card-header text-center pt-4">
+                                <img src="<?= base_url(); ?>assets_adm/img/logo/logo1.png" alt="Logo" width="50" height="50">
                                 <h5>Login</h5>
                             </div>
                             <div class="card">
